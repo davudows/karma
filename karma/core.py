9a2193bba72a454d37d183676952b88587e6fc4e
decoxviii <46903940+decoxviii@users.noreply.github.com>
2019-05-25 12:45:03 -0500

    logging module was added

--- karma/core.py
+++ karma/core.py
@@ -1,192 +1,182 @@
 #!/usr/bin/env python3
 
+import logging
 import sys
 import re
 import os
 
-# Colors
-GREEN, RED, RESET = '\033[92m', '\033[91m', '\033[0m'
-
 try:
+    from karma import formatter
     import requests
 except Exception as e:
-    print('Error:', e)
-    print("Please install the requirements:\n\t$ sudo -H pip3 install -r requirements.txt")
-    sys.exit(1)
+    raise e
+
+
+def get_logger():
+
+    global logger
+    logger = logging.getLogger("Karma")
+
 
 class pwndb(object):
 
     """Docstring for pwndb. """
 
     def __init__(self, args):
-        self.domain = 'http://pwndb2am4tzkvold.onion/'
+
+        get_logger()
+        self.site = "http://pwndb2am4tzkvold.onion/"
         self.args = args
-        self.data = {
-            'luseropr'  : 0,
-            'domainopr' : 0,
-            'submitform': 'em'
-        }
-        
-        proxy = self.args['--proxy']
-        if '//' in proxy:
-            proxy = proxy.split('//')[1]
+        self.data = {"luseropr": 0, "domainopr": 0, "submitform": "em"}
+
+        proxy = self.args["--proxy"]
+        if "//" in proxy:
+            proxy = proxy.split("//")[1]
 
         self.session = requests.session()
         self.session.proxies = {
-            'http' : 'socks5h://%s' % proxy,
-            'https': 'socks5h://%s' % proxy
+            "http": "socks5h://%s" % proxy,
+            "https": "socks5h://%s" % proxy,
         }
-
-
 
     def get_request(self, data):
         """ Get requests """
-        
+
         try:
-            req = self.session.post(self.domain, data=data, timeout=(15, None))
-        
-        except requests.exceptions.ConnectTimeout as e:
-            print('{}Error: {}{}'.format(RED, e, RESET))
-            print("{}> The site: {} is down, try again later{}".format(GREEN, self.domain, RESET))
+            req = self.session.post(self.site, data=data, timeout=(15, None))
+
+        except requests.exceptions.ConnectTimeout as error:
+            logger.error(error)
+            logger.info("the site: {} is down, try again later".format(self.site))
             sys.exit(1)
 
-        except requests.exceptions.ConnectionError as e:
-            print('{}Error: {}{}'.format(RED, e, RESET))
-            print("{}> Please restart the tor service and try again. Run:\n".format(GREEN))
-            print("\t$ sudo service tor restart{}\n".format(RESET))
-            sys.exit(2)
+        except requests.exceptions.ConnectionError as error:
+            logger.error(error)
+            logger.info("please restart the tor service and try again")
+            sys.exit(1)
 
         except Exception as e:
             raise e
-        
-        return req.text
 
+        return req.text
 
     def response_parser(self, response):
         """ Parse pwndb response """
 
-        print("\n{}> Analyzing response{}".format(GREEN, RESET))
-
         if not response:
-            return ''
+            logger.warn("no results were obtained")
+            sys.exit(1)
 
-        resp = re.findall(r'\[(.*)', response)
-        resp = [ resp[n:n+4] for n in range(0, len(resp), 4) ]
+        logger.info("analyzing results:")
+        resp = re.findall(r"\[(.*)", response)
+        resp = [resp[n : n + 4] for n in range(0, len(resp), 4)]
+
         results = {}
-        getinfo = lambda s: s.split('=>')[1].strip()
+        getinfo = lambda s: s.split("=>")[1].strip()
         for item in resp:
             results[getinfo(item[0])] = {
-                'email':'{}@{}'.format(getinfo(item[1]), getinfo(item[2])),
-                'passw': getinfo(item[3])
+                "email": "{}@{}".format(getinfo(item[1]), getinfo(item[2])),
+                "passw": getinfo(item[3]),
             }
 
         return results
-    
-       
-    def email_request(self, target):
+
+    def email_request(self, target, num_targets=1, i=1):
         """ Request with email """
-        
+
         regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
         if re.match(regex, target):
-            print('{}> Request email: {}{}\033[J'.format(GREEN, target, RESET), end='\r')
-
-            self.data['luser']  = target.split('@')[0]
-            self.data['domain'] = target.split('@')[1]
-
+            logger.info("{}/{} request email: {}".format(i, num_targets, target))
+            self.data["luser"] = target.split("@")[0]
+            self.data["domain"] = target.split("@")[1]
             return self.get_request(self.data)
-        
-        print('{}> Invalid email: {}{}\033[J'.format(RED, target, RESET))
-        return ""
 
-    
-    def search_localpart(self, target):
+        else:
+            logger.warn("invalid email: {}".format(target))
+            return ""
+
+    def search_localpart(self, target, num_targets=1, i=1):
         """ Request with localpart """
 
         regex = r"(^[a-zA-Z0-9_.+%-]+$)"
         if re.match(regex, target):
-            print('{}> Request local-part: {}{}\033[J'.format(GREEN, target, RESET), end='\r')
-
-            self.data['luseropr'] = 1
-            self.data['luser'] = target
-
+            logger.info("{}/{} request local-part: {}".format(i, num_targets, target))
+            self.data["luseropr"] = 1
+            self.data["luser"] = target
             return self.get_request(self.data)
 
-        print('{}> Invalid local-part: {}{}\033[J'.format(RED, target, RESET))
-        return ""
+        else:
+            logger.warn("invalid local-part: {}".format(target))
+            return ""
 
-
-    def search_domain(self, target):
+    def search_domain(self, target, num_targets=1, i=1):
         """ Requests with domain """
 
         regex = r"(^[a-zA-Z0-9-%]+\.[a-zA-Z0-9-.%]+$)"
         if re.match(regex, target):
-            print('{}> Request domain: {}{}\033[J'.format(GREEN, target, RESET), end='\r')
-
-            self.data['domainopr'] = 1
-            self.data['domain'] = target
-
+            logger.info("{}/{} request domain: {}".format(i, num_targets, target))
+            self.data["domainopr"] = 1
+            self.data["domain"] = target
             return self.get_request(self.data)
 
-        print('{}> Invalid domain: {}{}\033[J'.format(RED, target, RESET))
-        return ""
+        else:
+            logger.warn("invalid domain: {}".format(target))
+            return ""
 
-
-    def search_password(self, target):
+    def search_password(self, target, num_targets=1, i=1):
         """ Requests with password """
-        
-        print('{}> Request password: {}{}\033[J'.format(GREEN, target, RESET), end='\r')
-            
-        self.data['submitform'] = 'pw'
-        self.data['password'] = target
 
+        logger.info("{}/{} request password: {}".format(i, num_targets, target))
+        self.data["submitform"] = "pw"
+        self.data["password"] = target
         return self.get_request(self.data)
 
-
-    def choose_function(self, target):
+    def choose_function(self, target, num_targets=1, i=1):
         """
         Choose the corresponding function 
         according to the parameter
         """
 
         opts = {
-            '--local-part': self.search_localpart,
-            '--password'  : self.search_password,
-            '--domain'    : self.search_domain
+            "--local-part": self.search_localpart,
+            "--password": self.search_password,
+            "--domain": self.search_domain,
         }
-            
+
         for key, value in self.args.items():
             if value and key in opts:
-                return opts[key](target)
-
+                return opts[key](target, num_targets, i)
 
     def search_info(self):
         """Start the information search"""
 
-        opt_search = self.args['search']
-        opt_target = self.args['target']
-        
-        response = ''
-        target = self.args['<target>']
+        opt_search = self.args["search"]
+        opt_target = self.args["target"]
+        target = self.args["<target>"]
 
+        response = ""
         if os.path.exists(target):
-            lines = open(target, 'r').readlines()
-            for item in lines:
-                item = item.strip('\n')
+
+            targets = open(target, "r").readlines()
+            num_targets = len(targets)
+
+            for i, target in enumerate(targets, 1):
+                target = target.strip("\n")
                 try:
                     if opt_search:
-                        response += self.choose_function(item)
+                        response += self.choose_function(target, num_targets, i)
+
                     if opt_target:
-                        response += self.email_request(item)
+                        response += self.email_request(target, num_targets, i)
+
                 except KeyboardInterrupt:
-                    print('\n{}> break{}'.format(RED, RESET))
+                    logger.warn("search has stopped")
                     break
 
-            return self.response_parser(response)
-        
-        if opt_search:
+        elif opt_search:
             response = self.choose_function(target)
-        if opt_target and target:
-            response = self.email_request(target)
-        
-        return self.response_parser(response)
 
+        elif opt_target and target:
+            response = self.email_request(target)
+
+        return self.response_parser(response)
